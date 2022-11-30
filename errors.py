6a05561aab585a9e4b869e6dc49761ebc7d22b8c
LouieMartin <louie.decierdo@gmail.com>
2022-11-30 18:04:15 +0800

    Small updates

--- errors.py
+++ errors.py
@@ -1,7 +1,11 @@
 class IllegalCharacterError(Exception):
   def __init__(self, character):
-    super().__init__(f'Illegal Character: \'{character}\'')
+    self.message = f'Illegal Character: \'{character}\''
+
+    super().__init__(self.message)
 
 class MathError(Exception):
   def __init__(self, message):
-    super().__init__(f'Math Error: {message}')
+    self.message = f'Math Error {message}'
+
+    super().__init__(self.message)
